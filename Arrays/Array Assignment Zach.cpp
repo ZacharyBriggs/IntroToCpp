99a093b5c147883d806d3b56c90b1177e89c0ac8
ZacharyBriggs <zacharybriggs64@gmail.com>
2017-08-22 15:55:38 -0500

    array 2,3,4 and maybe 5 done

--- Arrays/Array Assignment Zach.cpp
+++ Arrays/Array Assignment Zach.cpp
@@ -22,11 +22,25 @@ int main()
 	char letters[26];
 		//26
 	//3. Declare an array with the values 1 - 10;
+	int values[10] = { 1,2,3,4,5,6,7,8,9,10 };
 	//4. Using the array created in problem 3 print the 5th index.
+	std::cout << values[4] << std::endl;
 	//5. Using a loop, populate an array of size 10 with the values 10 - 1.
+	int loopArray[10];
+	int iter = 0;
+	for (int a = 10; a >= 1; a--)
+	{
+		iter++;
+		loopArray[iter];
+	}
 	//6. Create a an array of size five then ask the user to input five 
 	//number into the array. Display the largest and smallest numbers 
 	//to the console.
+		int fiveGuys[5];
+		std::cout << "Put in 5 numbers.\n";
+		std::cin >> fiveGuys[5];
+		std::cout << fiveGuys[5] << std::endl;
+		system("pause");
 	//7. Create a program that creates a small 3x3 array of integers.Use a nested for 
 	//loop to initialise the numbers 1 - 9. Output the numbers in a grid format.
 	system("pause");
--- Tester/Test.cpp
+++ Tester/Test.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 int main()
 {
-
+	int fiveGuys[5];
+	std::cout << "Put in 5 numbers.\n";
+	std::cin >> fiveGuys[5];
+	std::cout << fiveGuys[5] << std::endl;
 	system("pause");
 }
 
