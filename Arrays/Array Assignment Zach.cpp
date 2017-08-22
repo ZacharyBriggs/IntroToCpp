54636a4ebfbdeb3cc6c33af035d6ac7a4e64e1a8
ZacharyBriggs <zacharybriggs64@gmail.com>
2017-08-22 15:35:30 -0500

    Arrays (Arraies?) 1 done

---
+++ Arrays/Array Assignment Zach.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+int main()
+{
+	//1. Declare an array for each of the following(size of array does not matter unless
+	//sepcified).
+	//a. heights of students
+	int heightsStudents[10];
+	//b. age of parents
+	int ageParents[25];
+	//c. true of false questions
+	bool trueFalseQuestions[10];
+	//d. letters of the alphabet
+	char alphabet[26];
+	//2. What is the size in memory of the following arrays
+	//a. 
+	float heights[5];
+		//20
+	//b. 
+	int ages[10];
+	//	40
+	//c. 
+	char letters[26];
+		//26
+	//3. Declare an array with the values 1 - 10;
+	//4. Using the array created in problem 3 print the 5th index.
+	//5. Using a loop, populate an array of size 10 with the values 10 - 1.
+	//6. Create a an array of size five then ask the user to input five 
+	//number into the array. Display the largest and smallest numbers 
+	//to the console.
+	//7. Create a program that creates a small 3x3 array of integers.Use a nested for 
+	//loop to initialise the numbers 1 - 9. Output the numbers in a grid format.
+	system("pause");
+}
