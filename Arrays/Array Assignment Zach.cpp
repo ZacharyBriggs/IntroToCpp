fcf7b5cc0cc5eddfeaf739ff088e7c9e49090f83
ZacharyBriggs <zacharybriggs64@gmail.com>
2017-08-22 16:09:31 -0500

    array 6 maybe done

--- Arrays/Array Assignment Zach.cpp
+++ Arrays/Array Assignment Zach.cpp
@@ -26,22 +26,29 @@ int main()
 	//4. Using the array created in problem 3 print the 5th index.
 	std::cout << values[4] << std::endl;
 	//5. Using a loop, populate an array of size 10 with the values 10 - 1.
-	int loopArray[10];
+	int tenThruOne[10];
 	int iter = 0;
 	for (int a = 10; a >= 1; a--)
 	{
 		iter++;
-		loopArray[iter];
+		tenThruOne[iter];
 	}
 	//6. Create a an array of size five then ask the user to input five 
 	//number into the array. Display the largest and smallest numbers 
 	//to the console.
-		int fiveGuys[5];
-		std::cout << "Put in 5 numbers.\n";
-		std::cin >> fiveGuys[5];
-		std::cout << fiveGuys[5] << std::endl;
+	int fiveGuys[5];
+	std::cout << "Put in 5 numbers.\n";
+
+	for (int i = 0; i < 5; i++)
+	{
+		int input = 0;
+		std::cin >> fiveGuys[input];
+		input++;
+	}
+	std::cout << fiveGuys[5] << std::endl;
 		system("pause");
 	//7. Create a program that creates a small 3x3 array of integers.Use a nested for 
 	//loop to initialise the numbers 1 - 9. Output the numbers in a grid format.
+		int grid[3][3];
 	system("pause");
 }
--- Tester/Test.cpp
+++ Tester/Test.cpp
@@ -3,7 +3,13 @@ int main()
 {
 	int fiveGuys[5];
 	std::cout << "Put in 5 numbers.\n";
-	std::cin >> fiveGuys[5];
+	
+	for (int i = 0; i < 5; i++)
+	{
+		int input= 0;
+		std::cin >> fiveGuys[input];
+		input++;
+	}
 	std::cout << fiveGuys[5] << std::endl;
 	system("pause");
 }
