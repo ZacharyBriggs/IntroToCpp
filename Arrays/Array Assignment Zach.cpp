2e49b38decba1511d432e8b542032584a59b7be9
ZacharyBriggs <zacharybriggs64@gmail.com>
2017-09-02 10:30:46 -0500

    array redone into functions
    still gotta change the dragon problem tho

--- Arrays/Array Assignment Zach.cpp
+++ Arrays/Array Assignment Zach.cpp
@@ -14,80 +14,50 @@ void QuestionTwo()
 }
 void QuestionThree()
 {
-
+	int values[10] = { 1,2,3,4,5,6,7,8,9,10 };
 }
 void QuestionFour()
 {
-
+	int values[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	std::cout << values[4] << std::endl;
 }
 void QuestionFive()
 {
-
-}
-void QuestionSix()
-{
-
-}
-void QuestionSeven()
-{
-
-}
-
-void main()
-{
-	std::cout << "Decalre an array for each of the following\n";
-	QuestionOne();
-	std::cout << " What is the size in memory of the following arrays\n";
-	QuestionTwo();
-	std::cout << "Decalre an array with values 1-10\n";
-	QuestionThree();
-	//3. Declare an array with the values 1 - 10;
-	int values[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	std::cout << "Using the array from 3 print the 5th index\n";
-	QuestionFour();
-	//4. Using the array created in problem 3 print the 5th index.
-	std::cout << values[4] << std::endl;
-	std::cout << "Using a loop, populate an array of 10 with values 10-1\n";
-	QuestionFive();
-	//5. Using a loop, populate an array of size 10 with the values 10 - 1.
-	int tenThruOne[10];
+	int countdown[10];
 	int iter = 9;
 	for (int a = 10; a >= 1; a--)
 	{
-		tenThruOne[iter] = a;
-		std::cout << tenThruOne[iter] << std::endl;
+		countdown[iter] = a;
+		std::cout << countdown[iter] << std::endl;
 		iter--;
 	}
-	std::cout << "Create an array of five, ask user to input five numbers, display the largest and smallest\n";
-	QuestionSix();
-	//6. Create a an array of size five then ask the user to input five 
-	//number into the array. Display the largest and smallest numbers 
-	//to the console.
+}
+void QuestionSix()
+{
 	int fiveGuys[5];
 	std::cout << "Put in 5 numbers.\n";
 	int input = 0;
-		for (int i = 0; i < 5; i++)
-		{
-			std::cin >> fiveGuys[input];
-			input++;
-		}
-		int bigNumber = fiveGuys[0];
-		int smallNumber = fiveGuys[0];
-		for (int valueOne = 0; valueOne <= 4; valueOne++)
-		{
-			if (fiveGuys[valueOne] < smallNumber)
-					smallNumber = fiveGuys[valueOne];
-			if (fiveGuys[valueOne] >= bigNumber)
-					bigNumber = fiveGuys[valueOne];
-		}
+	for (int i = 0; i < 5; i++)
+	{
+		std::cin >> fiveGuys[input];
+		input++;
+	}
+	int bigNum = fiveGuys[0];
+	int smallNum = fiveGuys[0];
+	for (int valueOne = 0; valueOne <= 4; valueOne++)
+	{
+		if (fiveGuys[valueOne] < smallNum)
+			smallNum = fiveGuys[valueOne];
+		if (fiveGuys[valueOne] >= bigNum)
+			bigNum = fiveGuys[valueOne];
+	}
 	std::cout << "Biggest number is...\n";
-	std::cout << bigNumber << std::endl;
+	std::cout << bigNum << std::endl;
 	std::cout << "Smallest number is...\n";
-	std::cout << smallNumber << std::endl;
-	std::cout << " Create a 3x3 array and use a loop to populate it. Output numbers in a grid format\n";
-	QuestionSeven();
-	//7. Create a program that creates a small 3x3 array of integers.Use a nested for 
-	//loop to initialise the numbers 1 - 9. Output the numbers in a grid format.
+	std::cout << smallNum << std::endl;
+}
+void QuestionSeven()
+{
 	int grid[3][3];
 	int row = 0;
 	int column = 0;
@@ -111,5 +81,23 @@ void main()
 		else if (iter == 9)
 			std::cout << std::endl;
 	}
+}
+
+void main()
+{
+	std::cout << "Decalre an array for each of the following\n";
+	QuestionOne();
+	std::cout << " What is the size in memory of the following arrays\n";
+	QuestionTwo();
+	std::cout << "Decalre an array with values 1-10\n";
+	QuestionThree();
+	std::cout << "Using the array from 3 print the 5th index\n";
+	QuestionFour();
+	std::cout << "Using a loop, populate an array of 10 with values 10-1\n";
+	QuestionFive();
+	std::cout << "Create an array of five, ask user to input five numbers, display the largest and smallest\n";
+	QuestionSix();
+	std::cout << " Create a 3x3 array and use a loop to populate it. Output numbers in a grid format\n";
+	QuestionSeven();
 	system("pause");
 }
