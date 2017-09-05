e17cab89e7d306122b81cd15a4ad6c1c9ad3c913
ZacharyBriggs <zacharybriggs64@gmail.com>
2017-09-05 16:41:07 -0500

    function start

---
+++ Functions/Function Assignment Zach.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+void questionOne()
+{
+
+}
+void questionTwo()
+{
+
+}
+void questionThree()
+{
+
+}
+void questionFour()
+{
+
+}
+void questionFive()
+{
+
+}
+void questionSix()
+{
+
+}
+//2.
+int Add(int a, int b)
+{
+	return a + b;
+}
+int Subtract(int a, int b)
+{
+	return a - b;
+}
+int Mult(int a, int b)
+{
+	return a * b;
+}
+int Divide(int a, int b)
+{
+	return a / b;
+}
+//3.
+void largeArray(float someArray[], int length)
+{
+
+}
+//4
+
+//1, 1, 2, 3, 5, 8, etc.
+
+//5
+void main()
+{
+	std::cout << Add(1, 2) << std::endl;
+	std::cout << Subtract(1, 1) << std::endl;
+	std::cout << Mult(2, 2) << std::endl;
+	std::cout << Divide(6, 2) << std::endl;
+}
