cd40b97a555116d5e77d93e07d2670439ad4f10c
ZacharyBriggs <zacharybriggs64@gmail.com>
2017-09-08 15:09:17 -0500

    replacing pieces done
    neither player can replace pieces but the game lasts forever

--- TicTacToe/TicTacToeSource.cpp
+++ TicTacToe/TicTacToeSource.cpp
@@ -7,6 +7,7 @@ having to relaunch the application.You must provide a document breaking down the
 solve the problem.
 */
 #include <iostream>
+int gridSpace;
 char space1;
 char space2;
 char space3;
@@ -16,16 +17,27 @@ char space6;
 char space7;
 char space8;
 char space9;
+void Player1();
+void Player2();
 void Board();
 void main()
 {
 	char gameGrid[3][3];
-	int gridSpace;
 	bool gameOver = false;
 	while (gameOver == false)
 	{
 		char gameGrid[3][3];
 		int gridSpace;
+		Player1();
+		Player2();
+	}
+	system("pause");
+}	
+void Player1()
+{
+	bool p1Turn = true;
+	while (p1Turn == true)
+	{
 		std::cout << "P1 choose a space.\n";
 		std::cin >> gridSpace;
 		if (gridSpace == 1)
@@ -33,72 +45,258 @@ void main()
 			if (space1 == 'O' || space1 == 'X')
 			{
 				std::cout << "Spot already taken.\n";
-				break;
+				continue;
 			}
 			else if (space1 != 'O' || space1 != 'X');
 			{
 				space1 = 'X';
+				break;
 			}
 		}
 		else if (gridSpace == 2)
-			space2 = 'X';
+		{
+			if (space2 == 'O' || space2 == 'X')
+			{
+				std::cout << "Spot already taken.\n";
+				continue;
+			}
+			else if (space2 != 'O' || space2 != 'X');
+			{
+				space2 = 'X';
+				break;
+			}
+		}
 		else if (gridSpace == 3)
-			space3 = 'X';
+		{
+			if (space3 == 'O' || space3 == 'X')
+			{
+				std::cout << "Spot already taken.\n";
+				continue;
+			}
+			else if (space3 != 'O' || space3 != 'X');
+			{
+				space3 = 'X';
+				break;
+			}
+		}
 		else if (gridSpace == 4)
-			space4 = 'X';
+		{
+			if (space4 == 'O' || space4 == 'X')
+			{
+				std::cout << "Spot already taken.\n";
+				continue;
+			}
+			else if (space4 != 'O' || space4 != 'X');
+			{
+				space4 = 'X';
+				break;
+			}
+		}
 		else if (gridSpace == 5)
-			space5 = 'X';
+		{
+			if (space5 == 'O' || space5 == 'X')
+			{
+				std::cout << "Spot already taken.\n";
+				continue;
+			}
+			else if (space5 != 'O' || space5 != 'X');
+			{
+				space5 = 'X';
+				break;
+			}
+		}
 		else if (gridSpace == 6)
-			space6 = 'X';
+		{
+			if (space6 == 'O' || space6 == 'X')
+			{
+				std::cout << "Spot already taken.\n";
+				continue;
+			}
+			else if (space6 != 'O' || space6 != 'X');
+			{
+				space6 = 'X';
+				break;
+			}
+		}
 		else if (gridSpace == 7)
-			space7 = 'X';
+		{
+			if (space7 == 'O' || space7 == 'X')
+			{
+				std::cout << "Spot already taken.\n";
+				continue;
+			}
+			else if (space7 != 'O' || space7 != 'X');
+			{
+				space7 = 'X';
+				break;
+			}
+		}
 		else if (gridSpace == 8)
-			space8 = 'X';
+		{
+			if (space8 == 'O' || space8 == 'X')
+			{
+				std::cout << "Spot already taken.\n";
+				continue;
+			}
+			else if (space8 != 'O' || space8 != 'X');
+			{
+				space8 = 'X';
+				break;
+			}
+		}
 		else if (gridSpace == 9)
-			space9 = 'X';
+		{
+			if (space9 == 'O' || space9 == 'X')
+			{
+				std::cout << "Spot already taken.\n";
+				continue;
+			}
+			else if (space9 != 'O' || space9 != 'X');
+			{
+				space9 = 'X';
+				break;
+			}
+		}
 		else
 		{
 			std::cout << "Not a valid space.\n";
+			continue;
 		}
-		Board();
+	}
+	Board();
+}
+void Player2()
+{
+	bool P2Turn = true;
+	while (P2Turn == true)
+	{
 		std::cout << "P2 choose a space.\n";
 		std::cin >> gridSpace;
 		if (gridSpace == 1)
-		{ 
-		if (space1 == 'O' || space1 == 'X')
 		{
-			std::cout << "Spot already taken.\n";
-			break;
-		}
-		else if (space1 != 'O' || space1 != 'X');
-		{
-			space1 = 'O';
-		}
+			if (space1 == 'O' || space1 == 'X')
+			{
+				std::cout << "Spot already taken.\n";
+				continue;
+			}
+			else if (space1 != 'O' || space1 != 'X');
+			{
+				space1 = 'O';
+				break;
+			}
 		}
 		else if (gridSpace == 2)
-			space2 = 'O';
+		{
+			if (space2 == 'O' || space2 == 'X')
+			{
+				std::cout << "Spot already taken.\n";
+				continue;
+			}
+			else if (space2 != 'O' || space2 != 'X');
+			{
+				space2 = 'O';
+				break;
+			}
+		}
 		else if (gridSpace == 3)
-			space3 = 'O';
+		{
+			if (space3 == 'O' || space3 == 'X')
+			{
+				std::cout << "Spot already taken.\n";
+				continue;
+			}
+			else if (space3 != 'O' || space3 != 'X');
+			{
+				space3 = 'O';
+				break;
+			}
+		}
 		else if (gridSpace == 4)
-			space4 = 'O';
+		{
+			if (space4 == 'O' || space4 == 'X')
+			{
+				std::cout << "Spot already taken.\n";
+				continue;
+			}
+			else if (space4 != 'O' || space4 != 'X');
+			{
+				space4 = 'O';
+				break;
+			}
+		}
 		else if (gridSpace == 5)
-			space5 = 'O';
+		{
+			if (space5 == 'O' || space5 == 'X')
+			{
+				std::cout << "Spot already taken.\n";
+				continue;
+			}
+			else if (space5 != 'O' || space5 != 'X');
+			{
+				space5 = 'O';
+				break;
+			}
+		}
 		else if (gridSpace == 6)
-			space6 = 'O';
+		{
+			if (space6 == 'O' || space6 == 'X')
+			{
+				std::cout << "Spot already taken.\n";
+				continue;
+			}
+			else if (space6 != 'O' || space6 != 'X');
+			{
+				space6 = 'O';
+				break;
+			}
+		}
 		else if (gridSpace == 7)
-			space7 = 'O';
+		{
+			if (space7 == 'O' || space7 == 'X')
+			{
+				std::cout << "Spot already taken.\n";
+				continue;
+			}
+			else if (space7 != 'O' || space7 != 'X');
+			{
+				space7 = 'O';
+				break;
+			}
+		}
 		else if (gridSpace == 8)
-			space8 = 'O';
+		{
+			if (space8 == 'O' || space8 == 'X')
+			{
+				std::cout << "Spot already taken.\n";
+				continue;
+			}
+			else if (space8 != 'O' || space8 != 'X');
+			{
+				space8 = 'O';
+				break;
+			}
+		}
 		else if (gridSpace == 9)
-			space9 = 'O';
+		{
+			if (space9 == 'O' || space9 == 'X')
+			{
+				std::cout << "Spot already taken.\n";
+				continue;
+			}
+			else if (space9 != 'O' || space9 != 'X');
+			{
+				space9 = 'O';
+				break;
+			}
+		}
 		else
 		{
 			std::cout << "Not a valid space.\n";
+			continue;
 		}
-		Board();
 	}
-	system("pause");
-}	
+	Board();
+}
 void Board()
 {
 	std::cout << " " << space1 << "|" << space2 << "|" << space3 << " \n";
