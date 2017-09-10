b8770b66a2f6ac1ca09636abf9be09739549d769
ZacharyBriggs <zacharybriggs64@gmail.com>
2017-09-10 12:27:17 -0500

    function assignment fin except for 4

--- Functions/Function Assignment Zach.cpp
+++ Functions/Function Assignment Zach.cpp
@@ -4,10 +4,10 @@ int QuestionTwoAdd(int a, int b);
 int QuestionTwoSubtract(int a, int b);
 int QuestionTwoMult(int a, int b);
 int QuestionTwoDivide(int a, int b);
-float QuestionThree(float someArray[], int length);
+void QuestionThree(float someArray[], int length);
 void QuestionFour();
-float QuestionFiveHalf(int a);
-void QuestionSix();
+float QuestionFiveHalf(float a);
+void QuestionSix(int someArray[], int length);
 void main()
 {
 	std::cout << "Question 2\n";
@@ -16,12 +16,55 @@ void main()
 	std::cout << QuestionTwoMult(2, 2) << std::endl;
 	std::cout << QuestionTwoDivide(6, 2) << std::endl;
 	std::cout << "Question 3\n";
-	float myArray[] = { 1,2,3,4,5 };
-	std::cout << QuestionThree(myArray, 5);
+	float myArray[5] = { 1,2,3,4,5 };
+	QuestionThree(myArray, 5);
+	std::cout << "Question 4\n";
 	std::cout << "Question 5\n";
 	std::cout << QuestionFiveHalf(5) << std::endl;
+	std::cout << "Question 6\n";
+	int theArray[5] = { 1, 2, 3, 4, 5 };
+	QuestionSix(theArray, 5);
+	system("pause");
 }
-//2.
+void questionOne()
+{
+//a.
+/*
+int sum(int x, int y)
+{
+int result;
+result = x + y;
+return result;
+}
+*/
+//b. 
+/*
+int sum(int n)
+{
+if (0 == n)
+return 0;
+else
+{
+n = n + n;
+return n;
+}
+}
+*/
+//c.
+/*
+#include <iostream>
+int square(int x);
+int main()
+{
+double x = 13.6;
+std::cout << "square of 13.6 = " << square(x) << std::endl;
+}
+int square(int x)
+{
+return x * x;
+}
+*/
+}
 int QuestionTwoAdd(int a, int b)
 {
 	return a + b;
@@ -38,25 +81,40 @@ int QuestionTwoDivide(int a, int b)
 {
 	return a / b;
 }
-//3.
-float QuestionThree(float someArray[], int length)
+void QuestionThree(float someArray[], int length)
 {
-	int bigNumber = someArray[0];
-	for (int iter = 0; iter < length; iter++)
+	float bigNumber = someArray[0];
+	for (int iter = 0; iter <= length; iter++)
 	{
 		if (someArray[iter] > bigNumber)
 			bigNumber = someArray[iter];
 	}
+	std::cout << bigNumber << std::endl;
+}
+void QuestionFour();
+{
+
 }
 //4
 
 //1, 1, 2, 3, 5, 8, etc.
-
-//5
-float QuestionFiveHalf(int a)
+float QuestionFiveHalf(float a)
 {
 	return a / 2;
 }
+void QuestionSix(int someArray[], int length)
+{
+	int total = 0;
+	for (int i = 0; i < length; i++)
+	{
+		total = someArray[i] + total;
+	}
+	std::cout << total << std::endl;
+}
+//1. Find the error in each of the following functions and explain how to fix them.
+//2. Create a function for each of the following math operators. They must return a value.
+//and take in at least two argument. Once all the functions have been created you will need to 
+//invoke them and print out the return.
 //3. Create a function that takes in two arguments, one beign an array of float and the other
 //being the size of the array. It must return the largest value in the array.
 //4. Using recursion write a function that prints out the Fibonacci sequence.
