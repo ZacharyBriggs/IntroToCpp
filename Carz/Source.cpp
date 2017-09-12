2452413c14ee468ffae835094a478a147a3473ec
ZacharyBriggs <zacharybriggs64@gmail.com>
2017-09-12 16:14:10 -0500

    more work on 9/12

--- Carz/Source.cpp
+++ Carz/Source.cpp
@@ -3,19 +3,12 @@
 int main()
 {
 	Vehicle classCars[7];
-	for (int i = 0; i < 0; i++)
+	
+	Vehicle mycar = mycar.CreateCar();
+
+	for (int i = 0; i < 7; i++)
 	{
-		Vehicle::CreateCar();
-		classCars[i] =
+		classCars[i] = classCars[i].CreateCar();
 	}
-	Vehicle zachCar;
-	zachCar.mTires = 4;
-	zachCar.mSeats = 5;
-	zachCar.mYear = 2015;
-	strcpy_s(zachCar.mMaker, "Ford\0");
-	strcpy_s(zachCar.mModel, "Focus\0");
-	zachCar.mEngine;
-	zachCar.EngineOn(false);
-	classCars [0] = zachCar;
 	system("pause");
 }
--- Carz/Vehicle.cpp
+++ Carz/Vehicle.cpp
@@ -9,8 +9,12 @@ Vehicle Vehicle::CreateCar()
 	Vehicle newCar;
 	std::cout << "Input amount of tires.\n";
 	std::cin >> newCar.mTires;
+	if (newCar.mTires < 0)
+		newCar.mTires = 0;
 	std::cout << "Input amount of seats.\n";
 	std::cin >> newCar.mSeats;
+	if (newCar.mSeats < 0)
+		newCar.mSeats = 0;
 	std::cout << "Input maker.\n";
 	std::cin >> newCar.mMaker;
 	std::cout << "Input model.\n";
@@ -19,3 +23,14 @@ Vehicle Vehicle::CreateCar()
 	std::cin >> newCar.mYear;
 	return newCar;
 }
+/*
+Vehicle zachCar;
+zachCar.mTires = 4;
+zachCar.mSeats = 5;
+zachCar.mYear = 2015;
+strcpy_s(zachCar.mMaker, "Ford\0");
+strcpy_s(zachCar.mModel, "Focus\0");
+zachCar.mEngine;
+zachCar.EngineOn(false);
+classCars[0] = zachCar;
+*/
